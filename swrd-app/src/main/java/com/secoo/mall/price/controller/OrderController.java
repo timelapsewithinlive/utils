009289ed35fb3c79949593ff75dcb2d9ed1bbf07
Divinlin <m18501183083@163.com>
2019-12-18 21:31:01 +0800

    责任链

--- swrd-app/src/main/java/com/secoo/mall/price/controller/OrderController.java
+++ swrd-app/src/main/java/com/secoo/mall/price/controller/OrderController.java
@@ -4,21 +4,17 @@ import designpatterns.chain.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 public class OrderController {
     @Resource
     OrderService orderService;
 
-    @PostConstruct
-    public void init(){
-        System.out.println(5555555555555l);
-    }
-
-    @RequestMapping("/createOrder")
+    @GetMapping("/createOrder")
     public Object createOrder(){
 
         return orderService.mockedCreateOrder(0);
