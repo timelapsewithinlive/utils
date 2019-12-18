55211f807b30478d560d5ffcf1c3e35c5fe1d8ca
xinghonglin <xinghonglin@secoo.com>
2019-12-18 17:33:57 +0800

    责任链工具

--- swrd-utils/src/main/java/designpatterns/chain/OrderCommitHandler.java
+++ swrd-utils/src/main/java/designpatterns/chain/OrderCommitHandler.java
@@ -8,7 +8,7 @@ public class OrderCommitHandler extends AbstractHandler implements SynHandler {
     @Override
     public Response synHandle(Request request) {
         System.out.println("订单提交开始--线程ID："+Thread.currentThread().getId()+"--当前时间: "+System.currentTimeMillis());
-        Response resp = new Response(FlagEnum.SUCCESS,653422897);
+        Response resp = new Response(FlagEnum.SUCCESS,"订单号："+60149759441046l);
         if(true){
             //return new Response(FlagEnum.FAIL,"订单提交失败");
 
