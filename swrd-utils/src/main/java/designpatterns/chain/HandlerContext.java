505d174cdc36687c39df787cd7f7817be5d68bf5
xinghonglin <xinghonglin@secoo.com>
2020-01-03 12:37:09 +0800

    责任链优化

--- swrd-utils/src/main/java/designpatterns/chain/HandlerContext.java
+++ swrd-utils/src/main/java/designpatterns/chain/HandlerContext.java
@@ -110,8 +110,8 @@ public class HandlerContext {
                 }
             } catch (Throwable e) {
                 //future获取response超时，或者response为空会出现异常
-                ctx.response=new Response(HandlerCurrentlyStatus.FAIL,null);
-                ctx.response.setCause(e);
+                //ctx.response=new Response(HandlerCurrentlyStatus.FAIL,null);
+                //ctx.response.setCause(e);
                 ctx.handler.exceptionCaught(ctx, e);
                 if(ctx.next!=null){
                     ctx.next.response=ctx.response;
