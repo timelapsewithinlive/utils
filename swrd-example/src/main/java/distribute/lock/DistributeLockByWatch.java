5c8d4448a9f101fb188df130cc5f149851da538f
xinghonglin <xinghonglin@secoo.com>
2020-01-03 14:11:55 +0800

    责任链优化

--- swrd-example/src/main/java/distribute/lock/DistributeLockByWatch.java
+++ swrd-example/src/main/java/distribute/lock/DistributeLockByWatch.java
@@ -8,7 +8,6 @@ package distribute.lock;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.Response;
 import redis.clients.jedis.Transaction;
 
 import java.net.InetAddress;
