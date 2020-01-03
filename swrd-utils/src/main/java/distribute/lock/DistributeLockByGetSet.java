fda296e53a0b67f2d507363d92436baff8653ae3
xinghonglin <xinghonglin@secoo.com>
2020-01-03 10:06:10 +0800

    分布式锁

--- swrd-utils/src/main/java/distribute/lock/DistributeLockByGetSet.java
+++ swrd-utils/src/main/java/distribute/lock/DistributeLockByGetSet.java
@@ -48,7 +48,7 @@ public class DistributeLockByGetSet {
             if (currentValueStr != null && Long.parseLong(currentValueStr.split(seperator)[0]) < System.currentTimeMillis()) {
                 String oldValueStr = jedis.getSet(key,expires+ seperator + hostAddress+seperator+ threadMark);//存在两个线程同时运行到这里。分别先后修改key值得情况。会放入其它线程的UUID标识。释放锁，就会无法释放，必须等待锁超时
                 if (oldValueStr != null && oldValueStr.equals(currentValueStr)) {
-                    jedis.set(key,expires+ seperator + hostAddress+seperator+ threadMark);//38 到 40行整个算一个cas操作
+                    jedis.set(key,expires+ seperator + hostAddress+seperator+ threadMark);//49 到 51行整个算一个cas操作
                     KEY_MAP_THREAD_MARK.put(key,threadMark);
                     return true;
                 }
