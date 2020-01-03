bf0e7b62c8e6a7d1c63a3f6c92c91a23dd251655
xinghonglin <xinghonglin@secoo.com>
2020-01-03 16:44:09 +0800

    责任链优化

--- swrd-example/src/main/java/jdk/concurrent/excutor/TestExcutor.java
+++ swrd-example/src/main/java/jdk/concurrent/excutor/TestExcutor.java
@@ -5,16 +5,27 @@ import java.util.concurrent.*;
 public class TestExcutor {
 
     static ExecutorService executor = Executors.newFixedThreadPool(3);
-    private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 5,60000, TimeUnit.MILLISECONDS,new LinkedBlockingDeque<Runnable>(1),new ThreadPoolExecutor.DiscardPolicy());
+    private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1,60000, TimeUnit.MILLISECONDS,new SynchronousQueue<>(),new RejectedExecutionHandler() {
+        //线程池满时用当前线程处理任务
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            System.out.println("线程池满，走拒绝策略");
+        }
+    });
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        Callable<String> callable = new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                System.out.println("线程执行");
-                return "返回值";
-            }
-        };
-        test(callable);
+        while (true){
+            Callable<String> callable = new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    //Thread.sleep(5000);
+                    System.out.println("线程执行");
+                    return "返回值";
+                }
+            };
+            test(callable);
+        }
+
         //Future<String> submit = executor.submit(callable);
 
         //threadPoolExecutor.shutdown();
@@ -22,9 +33,9 @@ public class TestExcutor {
 
     public static void test (Callable callable) throws ExecutionException, InterruptedException {
         Future<String> submit =threadPoolExecutor.submit(callable);
-        System.out.println(submit.get());
-        System.out.println("activethread:"+threadPoolExecutor.getActiveCount());
-        threadPoolExecutor.shutdown();
+        //System.out.println(submit.get());
+        //System.out.println("activethread:"+threadPoolExecutor.getActiveCount());
+        //threadPoolExecutor.shutdown();
 
 
     }
