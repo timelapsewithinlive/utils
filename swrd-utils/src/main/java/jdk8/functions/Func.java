58e5e58f5eccd28cd42842e8b38ca1537613f98e
honglin.xhl <honglin.xhl@alibaba-inc.com>
2020-09-28 18:45:54 +0800

    jdk8函数测试

--- swrd-utils/src/main/java/jdk8/functions/Func.java
+++ swrd-utils/src/main/java/jdk8/functions/Func.java
@@ -25,6 +25,14 @@ public class Func<T> {
         return new Func<>(target);
     }
 
+    public <U> Func<U> f(Function<T, U> function) {
+        return new Func<>(function.apply(target));
+    }
+
+    public <P> Func<T> f(P param, BiFunction<T, P, T> function) {
+        return new Func<>(function.apply(target, param));
+    }
+
     public static <T> Func<T> fn() {
         return new Func();
     }
@@ -34,85 +42,52 @@ public class Func<T> {
         return (Func<T>) this;
     }
 
-    public <U> Func<U> f(Function<T, U> function) {
-        return new Func<>(function.apply(target));
-    }
-
-    public <P> Func<T> f(P param, BiFunction<T, P, T> function) {
-        return new Func<>(function.apply(target, param));
-    }
-
-    public <P> Func<T> with(P param, BiFunction<T, P, T> function) {
-        return when(param, (p) -> true, function);
+    public <P> Func<T> withBiF(P param, BiFunction<T, P, T> function) {
+        return whenBiF(param, (p) -> true, function);
     }
 
     public <P> Func<T> batchBiF(Collection<P> param, BiFunction<T, P, T> function) {
         for (P p : param) {
-            this.with(p, function);
+            this.withBiF(p, function);
         }
         return this;
     }
 
     public <P> Func<T> batchBiF(P[] param, BiFunction<T, P, T> function) {
         for (P p : param) {
-            this.with(p, function);
+            this.withBiF(p, function);
         }
         return this;
     }
 
-    public <P> Func<T> when(P param, boolean predicate, BiFunction<T, P, T> function) {
+    public <P> Func<T> whenF(P param, boolean predicate, Function<P, T> function) {
+        if (predicate) {
+            target = function.apply(param);
+        }
+        return this;
+    }
+
+    public <P> Func<T> whenF( boolean predicate, Function<Object, T> function) {
+        if (predicate) {
+            target = function.apply(param);
+        }
+        return this;
+    }
+
+    public <P> Func<T> whenBiF(P param, boolean predicate, BiFunction<T, P, T> function) {
         if (predicate) {
             target = function.apply(target, param);
         }
         return this;
     }
 
-    public <P> Func<T> when(Function<T,P> func, boolean predicate, BiFunction<T, P, T> function) {
+    public <P> Func<T> whenBiF(Function<T,P> func, boolean predicate, BiFunction<T, P, T> function) {
         if (predicate) {
             target = function.apply(target,func.apply(target));
         }
         return this;
     }
 
-    public <P> Func<T> whenDefault(Function<T,P> func, boolean predicate, BiConsumer<T, P> function) {
-        if (predicate) {
-             function.accept(target,func.apply(target));
-        }
-        return this;
-    }
-
-    /**
-     * 当断言predicate为true,绑定函数结果
-     *
-     * @param param
-     * @param predicate
-     * @param function
-     * @param <P>
-     * @return
-     */
-    public <P> Func<T> whenApply(P param, boolean predicate, Function<P, T> function) {
-        if (predicate) {
-            target = function.apply(param);
-        }
-        return this;
-    }
-
-    public <P> Func<T> whenApply( boolean predicate, Function<Object, T> function) {
-        if (predicate) {
-            target = function.apply(param);
-        }
-        return this;
-    }
-
-    /**
-     * 当断言predicate为true,绑定函数结果
-     *
-     * @param param
-     * @param predicate
-     * @param consumer
-     * @param <P>
-     * @return
-     */
     public <P> Func<T> whenC(P param, boolean predicate, Consumer<P> consumer) {
         if (predicate) {
             consumer.accept(param);
@@ -120,59 +95,66 @@ public class Func<T> {
         return this;
     }
 
-    public <P> Func<T> when(P param, boolean predicate, BiConsumer<T, P> function) {
+    public <P> Func<T> whenBiC(Function<T,P> func, boolean predicate, BiConsumer<T, P> function) {
+        if (predicate) {
+             function.accept(target,func.apply(target));
+        }
+        return this;
+    }
+
+    public <P> Func<T> whenBiC(P param, boolean predicate, BiConsumer<T, P> function) {
         if (predicate) {
             function.accept(target, param);
         }
         return this;
     }
 
-    public <P> Func<T> when(P param, Predicate<P> predicate, BiFunction<T, P, T> function) {
-        return when(param, predicate.test(param), function);
+    public <P> Func<T> whenBiF(P param, Predicate<P> predicate, BiFunction<T, P, T> function) {
+        return whenBiF(param, predicate.test(param), function);
     }
 
-    public <P> Func<T> when(P param, Predicate<P> predicate, BiConsumer<T, P> function) {
-        return when(param, predicate.test(param), function);
+    public <P> Func<T> whenBiC(P param, Predicate<P> predicate, BiConsumer<T, P> function) {
+        return whenBiC(param, predicate.test(param), function);
     }
 
-    public Func<T> when(boolean param, BiFunction<T, Boolean, T> function) {
-        return when(param, param, function);
+    public Func<T> whenBiF(boolean param, BiFunction<T, Boolean, T> function) {
+        return whenBiF(param, param, function);
     }
 
-    public Func<T> whenNot(boolean param, BiFunction<T, Boolean, T> function) {
-        return when(param, !param, function);
+    public Func<T> whenBiFNot(boolean param, BiFunction<T, Boolean, T> function) {
+        return whenBiF(param, !param, function);
     }
 
-    public <P> Func<T> notNull(P param, BiFunction<T, P, T> function) {
-        return when(param, param != null, function);
+    public <P> Func<T> notNullBiF(P param, BiFunction<T, P, T> function) {
+        return whenBiF(param, param != null, function);
     }
 
-    public Func<T> notEmpty(String param, BiFunction<T, String, T> function) {
-        return when(param, param != null && !param.isEmpty(), function);
+    public Func<T> notEmptyBiF(String param, BiFunction<T, String, T> function) {
+        return whenBiF(param, param != null && !param.isEmpty(), function);
     }
 
-    public <P> Func<T> notEmpty(Collection<P> param, BiFunction<T, Collection<P>, T> function) {
-        return when(param, param != null && !param.isEmpty(), function);
+    public <P> Func<T> notEmptyBiF(Collection<P> param, BiFunction<T, Collection<P>, T> function) {
+        return whenBiF(param, param != null && !param.isEmpty(), function);
     }
 
-    public <P> Func<T> notEmpty(P[] param, BiFunction<T, P[], T> function) {
-        return when(param, param != null && param.length > 0, function);
+    public <P> Func<T> notEmptyBiF(P[] param, BiFunction<T, P[], T> function) {
+        return whenBiF(param, param != null && param.length > 0, function);
     }
 
-    public Func<T> gt0(Number param, BiFunction<T, Number, T> function) {
-        return when(param, param != null && param.intValue() > 0, function);
+    public Func<T> gt0BiF(Number param, BiFunction<T, Number, T> function) {
+        return whenBiF(param, param != null && param.intValue() > 0, function);
     }
 
-    public Func<T> ge0(Number param, BiFunction<T, Number, T> function) {
-        return when(param, param != null && param.intValue() >= 0, function);
+    public Func<T> ge0BiF(Number param, BiFunction<T, Number, T> function) {
+        return whenBiF(param, param != null && param.intValue() >= 0, function);
     }
 
-    public Func<T> lt0(Number param, BiFunction<T, Number, T> function) {
-        return when(param, param != null && param.intValue() < 0, function);
+    public Func<T> lt0BiF(Number param, BiFunction<T, Number, T> function) {
+        return whenBiF(param, param != null && param.intValue() < 0, function);
     }
 
-    public Func<T> le0(Number param, BiFunction<T, Number, T> function) {
-        return when(param, param != null && param.intValue() <= 0, function);
+    public Func<T> le0BiF(Number param, BiFunction<T, Number, T> function) {
+        return whenBiF(param, param != null && param.intValue() <= 0, function);
     }
 
     public T f() {
