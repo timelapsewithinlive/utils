597f970cfb67a1ffa4d28f4d6f67cfba94de49d2
honglin.xhl <honglin.xhl@alibaba-inc.com>
2020-12-01 10:30:56 +0800

    flink测试

--- swrd-flink/src/main/java/StreamingWindowWordCountJava.java
+++ swrd-flink/src/main/java/StreamingWindowWordCountJava.java
@@ -1,4 +1,5 @@
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.RichFilterFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -10,10 +11,10 @@ public class StreamingWindowWordCountJava {
     public static void main(String[] args) throws Exception {
         //定义socket的端口号
         int port;
-        try{
+        try {
             ParameterTool parameterTool = ParameterTool.fromArgs(args);
             port = parameterTool.getInt("port");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.err.println("没有指定port参数，使用默认值9000");
             port = 9000;
         }
@@ -22,19 +23,42 @@ public class StreamingWindowWordCountJava {
         //连接socket获取输入的数据
         DataStreamSource<String> text = env.socketTextStream("127.0.0.1", port, "\n");
         //计算数据
-        DataStream<WordWithCount> windowCount = text.flatMap(new FlatMapFunction<String, WordWithCount>() {
-            @Override
-            public void flatMap(String value, Collector<WordWithCount> out) throws Exception {
-                String[] splits = value.split("\\s");
-                for (String word:splits) {
-                    out.collect(new WordWithCount(word,1L));
-                }
-            }
-        })//打平操作，把每行的单词转为<word,count>类型的数据
+        DataStream<WordWithCount> windowCount = text
+                .filter(new RichFilterFunction<String>() {
+                    @Override
+                    public boolean filter(String value) throws Exception {
+                        if (value.contains("ss")) {
+                            return true;
+                        }
+                        return false;
+                    }
+                })
+                .flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    @Override
+                    public void flatMap(String value, Collector<WordWithCount> out) throws Exception {
+                        System.out.println(value);
+                        String[] splits = value.split("\\s");
+                        //打平操作，把每行的单词转为<word,count>类型的数据
+                        for (String word : splits) {
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                }).filter(new RichFilterFunction<WordWithCount>() {
+
+                    @Override
+                    public boolean filter(WordWithCount value) throws Exception {
+                        // 满足总数小于300，写入数据库数据库
+                        if(value.count <300){
+                            //insert mysql
+                        }
+                        return true;
+                    }
+                })
                 //针对相同的word数据进行分组
                 .keyBy("word")
                 //指定计算数据的窗口大小和滑动窗口大小
-                .timeWindow(Time.seconds(10),Time.seconds(3))
+                .timeWindow(Time.seconds(10))
+                //.timeWindow(Time.seconds(10), Time.seconds(3))
                 .sum("count");
         //把数据打印到控制台,使用一个并行度
         windowCount.print().setParallelism(1);
@@ -45,10 +69,13 @@ public class StreamingWindowWordCountJava {
     /**
      * 主要为了存储单词以及单词出现的次数
      */
-    public static class WordWithCount{
+    public static class WordWithCount {
         public String word;
         public long count;
-        public WordWithCount(){}
+
+        public WordWithCount() {
+        }
+
         public WordWithCount(String word, long count) {
             this.word = word;
             this.count = count;
