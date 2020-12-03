afe92919c52c03d28fbcf0023c01ddc60656526f
honglin.xhl <honglin.xhl@alibaba-inc.com>
2020-12-03 10:24:00 +0800

    flink测试

--- swrd-flink/src/main/java/DspSampleTest.java
+++ swrd-flink/src/main/java/DspSampleTest.java
@@ -1,25 +1,27 @@
 import com.alibaba.fastjson.JSON;
-import dsp.DspConfig;
-import function.DspConfigSourceFunction;
 import org.apache.flink.api.common.functions.AggregateFunction;
-import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.RichFilterFunction;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.datastream.*;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.windowing.evictors.Evictor;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.Trigger;
 import org.apache.flink.streaming.api.windowing.triggers.TriggerResult;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.runtime.operators.windowing.TimestampedValue;
 import org.apache.flink.util.Collector;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -41,7 +43,7 @@ public class DspSampleTest {
                 BasicTypeInfo.STRING_TYPE_INFO,
                 BasicTypeInfo.STRING_TYPE_INFO);
         //1.2广播dsp配置
-        BroadcastStream<DspConfig> broadcastStream = env.addSource(new DspConfigSourceFunction()).broadcast(CONFIG_DESCRIPTOR);
+        // BroadcastStream<DspConfig> broadcastStream = env.addSource(new DspConfigSourceFunction()).broadcast(CONFIG_DESCRIPTOR);
 
         DataStreamSource<String> text = env.addSource(new SourceFunction<String>() {
             private volatile boolean isRunning = true;
@@ -50,7 +52,7 @@ public class DspSampleTest {
             @Override
             public void run(SourceContext<String> sourceContext) throws Exception {
                 while (isRunning) {
-                    TimeUnit.MILLISECONDS.sleep(100);
+                    TimeUnit.MILLISECONDS.sleep(2000);
                     DspIdea dspIdea = new DspIdea(1L, random.nextLong());
                     sourceContext.collect(JSON.toJSONString(dspIdea));
                 }
@@ -83,7 +85,7 @@ public class DspSampleTest {
                         out.collect(dspIdea);
                     }
                 });
-        windowCount.connect(broadcastStream).process(new BroadcastProcessFunction() {
+      /*  windowCount.connect(broadcastStream).process(new BroadcastProcessFunction() {
             @Override
             public void processElement(Object o, ReadOnlyContext readOnlyContext, Collector collector) throws Exception {
                 System.out.println(o);
@@ -93,17 +95,17 @@ public class DspSampleTest {
             public void processBroadcastElement(Object o, Context context, Collector collector) throws Exception {
 
             }
-        });
+        });*/
 
         //3.定义窗口和触发器
         WindowedStream<DspIdea, Long, TimeWindow> windowedStream = windowCount.keyBy(DspIdea::getDspId)
                 //指定计算数据的窗口大小和滑动窗口大小
-                .timeWindow(Time.seconds(10))
+                .timeWindow(Time.seconds(1000))
                 .trigger(new Trigger<DspIdea, TimeWindow>() {
 
                     @Override
                     public TriggerResult onElement(DspIdea dspIdeaDspTuple2, long l, TimeWindow timeWindow, TriggerContext triggerContext) throws Exception {
-                        return TriggerResult.CONTINUE;
+                        return TriggerResult.FIRE;
                     }
 
                     @Override
@@ -119,6 +121,22 @@ public class DspSampleTest {
                     @Override
                     public void clear(TimeWindow timeWindow, TriggerContext triggerContext) throws Exception {
 
+                    }
+                }).evictor(new Evictor<DspIdea, TimeWindow>() {
+                    @Override
+                    public void evictBefore(Iterable<TimestampedValue<DspIdea>> iterable, int i, TimeWindow timeWindow, EvictorContext evictorContext) {
+                        //System.out.println(iterable);
+                    }
+
+                    @Override
+                    public void evictAfter(Iterable<TimestampedValue<DspIdea>> iterable, int i, TimeWindow timeWindow, EvictorContext evictorContext) {
+                        Iterator<TimestampedValue<DspIdea>> iterator = iterable.iterator();
+                        while (iterator.hasNext()) {
+
+                            TimestampedValue<DspIdea> next = iterator.next();
+                            DspIdea value = next.getValue();
+                            value = null;
+                        }
                     }
                 });
 
@@ -136,8 +154,13 @@ public class DspSampleTest {
                         if (accumulator.entityIds == null) {
                             accumulator.entityIds = new ArrayList<>();
                         }
-                        accumulator.entityIds.add(value.entityId);
-                        //System.out.println("accumulator:" + accumulator.toString());
+                        if (accumulator.dspIdeas == null) {
+                            accumulator.dspIdeas = new ArrayList<>();
+                        }
+                        accumulator.count += 1;
+                        //accumulator.entityIds.add(value.entityId);
+                        accumulator.dspIdeas.add(value);
+                        System.out.println("accumulator:" + accumulator.toString());
                         return accumulator;
                     }
 
@@ -155,25 +178,25 @@ public class DspSampleTest {
         );
         //5. 结果输出
         aggregate.addSink(
-                 new RichSinkFunction<Dsp>() {
+                new RichSinkFunction<Dsp>() {
 
-                     //连接资源
-                     @Override
-                     public void open(Configuration parameters) throws Exception {
-                     }
+                    //连接资源
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                    }
 
-                     @Override
-                     public void invoke(Dsp value, Context context) throws Exception {
-                         System.out.println("sink-------"+value);
-                     }
+                    @Override
+                    public void invoke(Dsp value, Context context) throws Exception {
+                        System.out.println("sink-------" + value);
+                    }
 
-                     //关闭资源、释放资源
-                     @Override
-                     public void close() throws Exception {
+                    //关闭资源、释放资源
+                    @Override
+                    public void close() throws Exception {
                         // System.out.println("closesssss");
-                     }
+                    }
 
-                 });
+                });
         //注意：因为flink是懒加载的，所以必须调用execute方法，上面的代码才会执行
         env.execute("streaming dsp sample");
     }
@@ -218,7 +241,9 @@ public class DspSampleTest {
 
     public static class Dsp {
         public Long dspId;
+        public int count;
         public List<Long> entityIds;
+        public List<DspIdea> dspIdeas;
 
         public Dsp() {
         }
@@ -232,7 +257,9 @@ public class DspSampleTest {
         public String toString() {
             return "DspIdea{" +
                     "dspId='" + dspId + '\'' +
+                    "count='" + count + '\'' +
                     ", entityIds=" + JSON.toJSONString(entityIds) +
+                    ", dspIdeas=" + JSON.toJSONString(dspIdeas) +
                     '}';
         }
     }
