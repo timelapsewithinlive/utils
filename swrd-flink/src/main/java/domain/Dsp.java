008c0576f0c95ed33ce66980542a1ada0e3f9711
honglin.xhl <honglin.xhl@alibaba-inc.com>
2020-12-03 10:59:37 +0800

    flink测试

---
+++ swrd-flink/src/main/java/domain/Dsp.java
@@ -0,0 +1,34 @@
+package domain;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.List;
+
+/**
+ * @author xinghonglin
+ * @date 2020/12/03
+ */
+public class Dsp {
+    public Long dspId;
+    public int count;
+    public List<Long> entityIds;
+    public List<DspIdea> dspIdeas;
+
+    public Dsp() {
+    }
+
+    public Dsp(Long dspId, List<Long> entityIds) {
+        this.dspId = dspId;
+        this.entityIds = entityIds;
+    }
+
+    @Override
+    public String toString() {
+        return "DspIdea{" +
+                "dspId='" + dspId + '\'' +
+                "count='" + count + '\'' +
+                ", entityIds=" + JSON.toJSONString(entityIds) +
+                ", dspIdeas=" + JSON.toJSONString(dspIdeas) +
+                '}';
+    }
+}
---
+++ swrd-flink/src/main/java/domain/DspIdea.java
@@ -0,0 +1,43 @@
+package domain;
+
+/**
+ * @author xinghonglin
+ * @date 2020/12/03
+ */
+public class DspIdea {
+
+    public Long dspId;
+    public Long entityId;
+
+    public DspIdea() {
+    }
+
+    public DspIdea(Long dspId, Long entityId) {
+        this.dspId = dspId;
+        this.entityId = entityId;
+    }
+
+    public Long getDspId() {
+        return dspId;
+    }
+
+    public void setDspId(Long dspId) {
+        this.dspId = dspId;
+    }
+
+    public Long getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(Long entityId) {
+        this.entityId = entityId;
+    }
+
+    @Override
+    public String toString() {
+        return "DspIdea{" +
+                "dspId='" + dspId + '\'' +
+                ", entityId=" + entityId +
+                '}';
+    }
+}
