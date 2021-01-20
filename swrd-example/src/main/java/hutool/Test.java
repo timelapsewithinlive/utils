aab209da0d458630154b5506e56fab03cb978e3e
honglin.xhl <honglin.xhl@alibaba-inc.com>
2021-01-20 22:00:39 +0800

    hutool工具测试

--- swrd-example/src/main/java/hutool/Test.java
+++ swrd-example/src/main/java/hutool/Test.java
@@ -8,7 +8,7 @@ import cn.hutool.core.date.DateUtil;
 /**
  * 说明链接：https://hutool.cn/docs/#/core/%E6%97%A5%E6%9C%9F%E6%97%B6%E9%97%B4/%E6%97%A5%E6%9C%9F%E6%97%B6%E9%97%B4%E5%B7%A5%E5%85%B7-DateUtil
  *
- * https://www.oschina.net/p/hutool?hmsr=aladdin1e1
+ * 开源：https://www.oschina.net/p/hutool?hmsr=aladdin1e1
  *
  * @author xinghonglin
  * @date 2021/01/20
